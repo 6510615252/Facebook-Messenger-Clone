6b72547235431dde650408c635bbaea6719e8d14
Folkchayutpong <folkchayutpong@gmail.com>
2026-03-28 13:37:01 +0700

    add deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
