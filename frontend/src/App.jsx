4c90f50d335101e04827eb7d18836ea3621bb30c
Peerapol Pochaoren <110813888+6510615252@users.noreply.github.com>
2026-04-20 17:01:42 +0700

    Change router from HashRouter to BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import WelcomePage from "./pages/WelcomePage";
